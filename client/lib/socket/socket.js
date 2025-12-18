87410410a94d92761d06718e078df753348d2991
SayeGorgor <sgorgor02@gmail.com>
2025-12-17 21:38:14 -0500

    update socket url to production server

--- client/lib/socket/socket.js
+++ client/lib/socket/socket.js
@@ -3,6 +3,13 @@
 import { createContext } from "react";
 import { io } from "socket.io-client";
 
-export const socket = io('http://localhost:8080', { autoConnect: true});
+export const socket = io(
+    process.env.NEXT_PUBLIC_SOCKET_URL, 
+    { 
+        autoConnect: true,
+        withCredentials: true,
+        transports: ['websocket']
+    }
+);
 
 export const SocketContext = createContext(socket);
